a39f800ea78810a33d7b795b5206c70d8d4cd065
Nikolay Parshakov <prognik97@gmail.com>
2021-02-03 15:48:57 +0300

    create slim basic app

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-Docker environment with docker-compose
-
-Services:
-1. nginx
-2. php-fom
-3. mysql
+Slim skeleton
--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo 'test';
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('test');
+    return $response;
+});
+
+$app->run();
